3fa0d6296faa4f33dc608b8ee69841f3a261db1f
Your Name <you@example.com>
2025-08-26 19:18:26 +0530

    Stack using Linked List(Structure)

---
+++ Stack_Using_LinkedList.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>  
+struct node {
+    int val;
+    struct node *add;
+};
+
+// Function to insert (push) an element into stack
+struct node *insert(struct node *head, int item) {
+    struct node *temp;
+    
+    if (head == NULL) {
+        head = (struct node *)malloc(sizeof(struct node));
+        head->val = item;
+        head->add = NULL;
+    } 
+	else {
+        temp = (struct node *)malloc(sizeof(struct node));
+        temp->val = item;
+        temp->add = head;
+        head = temp;
+    }
+    return head;
+}
+
+// Function to display stack elements
+void display(struct node *head) {
+    if (head == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+
+    
+    printf("Stack elements: ");
+    while (head != NULL) {
+        printf("%d ", head->val);
+        head = head->add;
+    }
+    printf("\n");
+}
+
+struct node *delete(struct node *head) {
+    struct node *temp;
+    if (head == NULL) {
+        printf("Stack is empty..\n");
+        return head;  
+    } else {
+        temp = head;
+        printf("Popped: %d\n", head->val);
+        head = head->add;
+        free(temp);
+    }
+    return head;
+}
+
+int main() {
+    struct node *head = NULL;
+    int choice, item;
+
+    do {
+        printf("\n--- Stack Menu ---\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &item);
+                head = insert(head, item);
+                break;
+            case 2:
+                head = delete(head);
+                break;
+            case 3:
+                display(head);
+                break;
+            case 4:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
